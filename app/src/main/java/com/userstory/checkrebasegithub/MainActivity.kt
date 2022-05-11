c414a9fc99f81d7951698bfe48fc2063a6d2dd3a
koen08 <konstantin.butusov2017@yandex.ru>
2022-05-11 12:45:01 +0700

    resul5tr

--- app/src/main/java/com/userstory/checkrebasegithub/MainActivity.kt
+++ app/src/main/java/com/userstory/checkrebasegithub/MainActivity.kt
@@ -2,6 +2,7 @@ package com.userstory.checkrebasegithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val x = 1 + 1
+        val y = 1 + 3
+        Log.e("TAG1", (x + y).toString())
     }
 }
